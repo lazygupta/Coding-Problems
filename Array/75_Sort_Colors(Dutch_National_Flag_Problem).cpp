74927aea895c279ecc6f3dd25598e6ecf2c3b555
lazygupta <guptashirley02@gmail.com>
2023-09-11 22:15:48 +0530

    .

--- Array/75_Sort_Colors(Dutch_National_Flag_Problem).cpp
+++ Array/75_Sort_Colors(Dutch_National_Flag_Problem).cpp
@@ -4,30 +4,39 @@
 
 // Better Force solution is O(N) + O(N) = O(2N)
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& arr) {
-    int cnt0=0,cnt1=0,cnt2=0;
-    for(int i=0;i<arr.size();i++){
-        if(arr[i]==0) cnt0++;
-        if(arr[i]==1) cnt1++;
-        if(arr[i]==2) cnt2++;
-    }
+    void sortColors(vector<int> &arr)
+    {
+        int cnt0 = 0, cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < arr.size(); i++)
+        {
+            if (arr[i] == 0)
+                cnt0++;
+            if (arr[i] == 1)
+                cnt1++;
+            if (arr[i] == 2)
+                cnt2++;
+        }
 
-    for(int i=0;i<cnt0;i++){
-        arr[i]=0;
-    }
+        for (int i = 0; i < cnt0; i++)
+        {
+            arr[i] = 0;
+        }
 
-    for(int i=cnt0;i<cnt0+cnt1;i++){
-        arr[i]=1;
-    }
+        for (int i = cnt0; i < cnt0 + cnt1; i++)
+        {
+            arr[i] = 1;
+        }
 
-    for(int i=cnt0+cnt1;i<arr.size();i++){
-        arr[i]=2;
-    }
+        for (int i = cnt0 + cnt1; i < arr.size(); i++)
+        {
+            arr[i] = 2;
+        }
     }
 };
 
@@ -41,25 +50,30 @@ public:
 
 // Click here to know about algorithm https://drive.google.com/file/d/1V67tC_jQuVA1iR1RlIKhh_pDeTHorhiX/view?usp=sharing
 
-
-class Solution {
+class Solution
+{
 public:
-    void sortColors(vector<int>& arr) {
-    int low=0,mid=0;
-    int high= arr.size()-1;
-    while(mid<=high){
-        if(arr[mid]==0){
-            swap(arr[low],arr[mid]);
-            low++;
-            mid++;
-        }
+    void sortColors(vector<int> &arr)
+    {
+        int low = 0, mid = 0;
+        int high = arr.size() - 1;
+        while (mid <= high)
+        {
+            if (arr[mid] == 0)
+            {
+                swap(arr[low], arr[mid]);
+                low++;
+                mid++;
+            }
 
-        else if(arr[mid]==1) mid++;
+            else if (arr[mid] == 1)
+                mid++;
 
-        else {
-            swap(arr[mid],arr[high]);
-            high--;
+            else
+            {
+                swap(arr[mid], arr[high]);
+                high--;
+            }
         }
-    }
     }
 };
