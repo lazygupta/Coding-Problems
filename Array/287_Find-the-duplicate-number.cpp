2f4c9e8fd29b0ab8c1b6e7b3dea00cad806aa71a
lazygupta <guptashirley02@gmail.com>
2023-09-13 20:15:11 +0530

    .

--- Array/287_Find-the-duplicate-number.cpp
+++ Array/287_Find-the-duplicate-number.cpp
@@ -1,3 +1,5 @@
+// The numbers in the array is in th erange [1-n] only.
+
 // T.C = O(N)
 // S.C = O(1)
 
@@ -7,12 +9,26 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        
         for(int i=0;i<nums.size();i++){
             int num = abs(nums[i]);
             if(nums[num]<0) return num;
             nums[num]= -nums[num];
         }
         return -1;
+    }
+};
+
+
+// Same queston 217. Contains Duplicate
+// The array has duplicate numbers
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> hset;
+        for(auto i:nums){
+            hset.insert(i);
+        }
+        return (nums.size()!=hset.size());
     }
 };
