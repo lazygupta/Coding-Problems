4c633408aa795f19dc0314434d63ee11d2fbb2f0
lazygupta <guptashirley02@gmail.com>
2023-09-16 01:10:06 +0530

    .

---
+++ practice.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        
+        for(auto x:nums2){
+            nums1.push_back(x);
+        }
+
+        sort(nums1.begin(),nums1.end());
+
+        int mid = nums1.size() / 2;
+
+        if(nums1.size() % 2 == 0) {
+            return ((nums1[mid] + nums1[mid-1])/2.0);
+        }
+
+        else return nums1[mid];
+    }
+};
