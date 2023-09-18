4a4df8b57a714724094aae992b5669d26fe0d16d
lazygupta <guptashirley02@gmail.com>
2023-09-18 13:40:30 +0530

    .

--- practice.cpp
+++ practice.cpp
@@ -1,50 +1,43 @@
-#define ll long long
-
-class Solution
+#include<bits/stdc++.h>
+using namespace std;
+int arya(int ar[],int n)
 {
-public:
-    int maximum(vector<int> &v)
+    // 2 2 4 5 6 8
+    // a = 
+    int a=0,b=0;
+    for(int i=0;i<n;i+=2)
     {
-        int maximum = 0;
-        for (int i = 0; i < v.size(); i++)
+        if(ar[i]%2!=0)//ar[4]
         {
-            maximum = max(v[i], maximum);
-        }
-        return maximum;
-    }
-
-    ll result(vector<int> &v, int hour_speed)
-    {
-        ll totalhrstaken = 0;
-        for (int i = 0; i < v.size(); i++)
-        {
-            totalhrstaken += ceil(double(v[i]) / double(hour_speed));
-        }
-        return totalhrstaken;
-    }
-
-    int minEatingSpeed(vector<int> &piles, int h)
-    {
-        int low = 1, high = maximum(piles);
-        while (low <= high)
-        {
-            int mid = low + (high - low) / 2;
-
-            if (result(piles, mid) == h)
-            {
-                return mid;
+            a+=ar[i];//1 => 8 => 11
+            if(ar[i+1]%2!=0){
+                b+=ar[i+1]; // 8 => 13
             }
-
-            if (result(piles, mid) > h)
-            {
-                low = mid + 1;
-            }
-
-            if (result(piles, mid) < h)
-            {
-                high = mid - 1;
+            else{
+                b+=ar[i+1];//2 => 8
+                reverse(ar+(i+1),ar+n);
             }
         }
-        return 99765;
+        else {
+            a+=ar[i];//2
+            reverse(ar+(i),ar+n);
+            if(ar[i+1]%2!=0){
+                b+=ar[i+1]; // 8 => 13
+            }
+            else{
+                b+=ar[i+1];
+                reverse(ar+(i+1),ar+n);
+            }
+        }
     }
-};
+    
+    return (a>b);
+}
+
+int main(){
+    int arr[6]={1,3,5,7,8,3};
+    if(arya(arr,6)){
+        cout<<"a is the winner"<<endl;
+    }
+    else cout<<"b is the winner"<<endl;
+}
