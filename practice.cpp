e5874736857c10b8d7e0883401378bffc8c5ccf9
Ankit <guptashirley02@gmail.com>
2023-10-13 15:44:33 +0530

    .

--- practice.cpp
+++ practice.cpp
@@ -2,21 +2,28 @@
 using namespace std;
 
 int main(){
-    int n,x;
-    cin>>n;
-    vector<int> v;
-    for(int i=0;i<n;i++){
-        cin>>x;
-        v.push_back(x);
-    }
-    int flag=0;
-    for(auto it:v){
-        if(it==1){
-            cout<<"HARD";
-            flag=1;
-            break;
+    int t;cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        if(x==y && y==z) cout<<"YES"<<endl;
+        else{
+            int f1=0,f2=0,f3=0;
+        while(x!=0){
+            x=x-(x/2);
+            if(x==1) f1=1;
+        }
+        while(y!=0){
+            y=y-(y/2);
+            if(y==1) f2=1;
+        }
+        while(z!=0){
+            z=z-(z/2);
+            if(z==1) f3=1;
+        }
+        if(f1==1 && f2==1 && f3==1) cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
         }
     }
-    if(flag==0) cout<<"EASY";
     return 0;
 }
