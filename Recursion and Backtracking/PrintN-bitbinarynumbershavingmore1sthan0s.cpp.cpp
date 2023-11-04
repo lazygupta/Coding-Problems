57fbd91e296cfd3792b10edfcd6876cfca17c2df
Ankit <guptashirley02@gmail.com>
2023-11-04 21:49:33 +0530

    .

---
+++ Recursion and Backtracking/PrintN-bitbinarynumbershavingmore1sthan0s.cpp.cpp
@@ -0,0 +1,75 @@
+// Mast question hai
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// First make recursive tree
+// Just write the same code you thought to draw recursive tree
+// Check for when there is double output and when there is single output
+
+class Solution{
+public:
+
+    void solve(vector<string> &result,int ones, int zeroes, int n, string output){
+        if(n==0){
+            result.push_back(output);
+            return;
+        }
+        if(ones>zeroes){
+            string op1=output;
+            string op2=output;
+            op1.push_back('1');
+            solve(result,ones+1,zeroes,n-1,op1);
+            op2.push_back('0');
+            solve(result,ones,zeroes+1,n-1,op2);
+        }
+        else if(ones==zeroes){
+            string op1=output;
+            op1.push_back('1');
+            solve(result,ones+1,zeroes,n-1,op1);
+        }
+    }
+
+	vector<string> NBitBinary(int N)
+	{
+	    vector<string> res;
+	    string output="";
+	    int ones=0,zeroes=0;
+	    solve(res,ones,zeroes,N,output);
+	    return res;
+	}
+};
+
+// further you can always push back 1 to string and whenever ones>zeroes you have to push back 0 too
+
+// So above code can be of shorter code optimization
+
+//User function template for C++
+class Solution{
+public:
+
+    void solve(vector<string> &result,int ones, int zeroes, int n, string output){
+        if(n==0){
+            result.push_back(output);
+            return;
+        }
+        string op1=output;
+        op1.push_back('1');
+        solve(result,ones+1,zeroes,n-1,op1);
+        if(ones>zeroes){
+            string op2=output;
+            op2.push_back('0');
+            solve(result,ones,zeroes+1,n-1,op2);
+        }
+    }
+
+	vector<string> NBitBinary(int N)
+	{
+	    vector<string> res;
+	    string output="";
+	    int ones=0,zeroes=0;
+	    solve(res,ones,zeroes,N,output);
+	    return res;
+	}
+};
