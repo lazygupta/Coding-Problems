8e8ea9251b54b14c4bb264e78061fcefffbd5882
Ankit <guptashirley02@gmail.com>
2023-11-17 20:55:05 +0530

    .

--- Recursion and Backtracking/39.CombinationSum.cpp
+++ Recursion and Backtracking/39.CombinationSum.cpp
@@ -5,22 +5,21 @@
 
 // I will call this as Verma's concept in Strivers way
 
-
-
+// T.C = O(2^t) x k
 
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
-
     void find(int index,int target,vector<int> &bucket,vector<int> candidates,vector<vector<int>> &res){
         if(index==candidates.size()){
             if(target==0)
             res.push_back(bucket);
             return;
         }
-        if(candidates[index]<=target){// this condition is because if the element is greater than the desired target then its of no use bcuz it can never fulfill teh desired target=0
+        if(candidates[index]<=target){ // this condition is because if the element is greater than the desired target 
+        // then its of no use bcuz it can never fulfill teh desired target=0
         bucket.push_back(candidates[index]);
         find(index,target-candidates[index],bucket,candidates,res);
         bucket.pop_back();
