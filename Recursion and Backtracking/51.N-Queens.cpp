a5ecbc42597e08a0a94fec72e39e8cee13a1a282
Ankit <guptashirley02@gmail.com>
2023-12-08 09:08:41 +0530

    .

---
+++ Recursion and Backtracking/51.N-Queens.cpp
@@ -0,0 +1,69 @@
+// Same intuition as Word Search
+// We have to fill Q in every row and then check for valid fill that it can accomodate Q there or not
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    bool validfill(int col,int row,vector<string> bucket,int n){
+        int duprow=row;
+        int dupcol=col;
+
+        //Checking Diagonally Upward Backward
+        while(row>=0 && col>=0){
+            if(bucket[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        //Checking Horizontally Backward
+        while(col>=0){
+            if(bucket[row][col] =='Q') return false;
+            col--;
+        }
+        row = duprow;
+        col = dupcol;
+
+        //Checking Diagonally Downward Backward
+        while(row<n && col>=0){
+            if(bucket[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+
+    void solve(int col,vector<vector<string>> &res,vector<string> &bucket,int n){
+        if(col==n){
+            res.push_back(bucket);
+            return;
+        }
+
+        int row=0;
+        for(row=0;row<n;row++){
+            if(validfill(col,row,bucket,n)){
+                bucket[row][col]='Q';
+                solve(col+1,res,bucket,n);
+                bucket[row][col]='.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> res;
+        vector<string> bucket(n);
+        string str(n,'.'); // Filling the string with '.'
+        for(int i=0;i<n;i++){
+            bucket[i]=str; //Filling the bucket with empty string of '.'
+        }
+        solve(0,res,bucket,n);
+        return res;
+    }
+};
---
+++ Recursion and Backtracking/79.WordSearch.cpp
@@ -0,0 +1,47 @@
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    bool findNext(int row,int col,int index,vector<vector<char>> &board,string word){
+        if(index==word.length()){
+            return true;
+        }
+        
+        if(row<0 || col<0 || row==board.size() || col==board[0].size() || board[row][col] != word[index] || board[row][col] == '#'){
+            return false;
+        }
+
+        char c= board[row][col];
+        board[row][col]='#';
+
+        bool bottom = findNext(row+1,col,index+1,board,word);
+        bool right = findNext(row,col+1,index+1,board,word);
+        bool top = findNext(row-1,col,index+1,board,word);
+        bool left = findNext(row,col-1,index+1,board,word);
+
+        board[row][col]=c;
+
+        return top || bottom || right || left;
+    }
+
+    bool exist(vector<vector<char>>& board, string word) {
+        int rows=board.size();
+        int cols = board[0].size();
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                if(board[i][j] == word[0]){
+                    if(findNext(i,j,0,board,word)){
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+};
