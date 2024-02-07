527c8e01248bc0fd97c8538998d2df6489f2cd7b
Ankit <guptashirley02@gmail.com>
2024-02-07 21:23:27 +0530

    .

--- Linked List/142. Linked List Cycle II.cpp
+++ Linked List/142. Linked List Cycle II.cpp
@@ -1,4 +1,5 @@
-
+// Hashing
+// S.C = O(N)
 
 
 #include<bits/stdc++.h>
@@ -21,6 +22,35 @@ public:
             }
             mpp[temp]++;
             temp=temp->next;
+        }
+        return NULL;
+    }
+};
+
+// using Tortoise and Hare method
+
+// S.C = O(N)
+
+// See the concept here = https://i.ibb.co/MMDpL1R/Screenshot-2024-02-07-211221.png
+
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head;
+        ListNode *fast = head;
+        while(fast!=NULL && fast->next != NULL){
+            // Finding Collission point 
+            slow = slow->next;// Slow ko ek step se le jaao
+            fast = fast->next->next; // Fast ko do step se le jaao
+
+            if(slow==fast){// Collission Area
+                slow=head;// Slow ko fir se suruat me rakh do
+                while(slow!=fast){ // Aur fir se slow ko bolo fast ke paas jaaye ek ek step se
+                    slow=slow->next;// Slow ek step se jayega
+                    fast=fast->next;// fast bhi ab ek hi step se jayega
+                }
+                return fast;
+            }
         }
         return NULL;
     }
