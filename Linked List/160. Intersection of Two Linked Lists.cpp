055d159504c8366196f9b33be114eee0bf407c35
Ankit <guptashirley02@gmail.com>
2024-02-18 16:08:35 +0530

    .

--- Linked List/160. Intersection of Two Linked Lists.cpp
+++ Linked List/160. Intersection of Two Linked Lists.cpp
@@ -69,3 +69,29 @@ public:
             return CollisionPoint(headB,headA,d);
     }
 };
+
+// T.C = O max(N or M)
+// S.C = O(1)
+
+class Solution {
+public:
+
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *dummy1=headA;
+        ListNode *dummy2=headB;
+        while(dummy1!=dummy2){
+            if(dummy1==NULL){
+                dummy1=headB;
+            }
+            else{
+                dummy1=dummy1->next;
+            }
+            if(dummy2==NULL){
+                dummy2=headA;
+            }
+            else
+                dummy2=dummy2->next;
+        }
+        return dummy1;
+    }
+};
