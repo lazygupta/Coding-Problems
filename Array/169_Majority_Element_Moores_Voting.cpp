2b4b05404fa6ce7b328998cd340825301914d6e4
lazygupta <guptashirley02@gmail.com>
2024-02-19 19:30:33 +0530

    .

--- Array/169_Majority_Element_Moores_Voting.cpp
+++ Array/169_Majority_Element_Moores_Voting.cpp
@@ -10,23 +10,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// class Solution {
-// public:
-//     int majorityElement(vector<int>& nums) {
-//         map<int,int> mpp;
-//         int mx=0;
-//         for(int i=0; i<nums.size() ;i++){
-//             mpp[nums[i]]++;
-//         }
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int> mpp;
+        int mx=0;
+        for(int i=0; i<nums.size() ;i++){
+            mpp[nums[i]]++;
+        }
 
-//         for(auto it:mpp){
-//             if(it.second > (nums.size()/2)){
-//                 return it.first;
-//             }
-//         }
-//         return 0;
-//     }
-// };
+        for(auto it:mpp){
+            if(it.second > (nums.size()/2)){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
 
 
 
