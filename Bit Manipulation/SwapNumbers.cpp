5b71b03c01dfffe2541c40bbd9dd7c9b0e099621
Ankit <guptashirley02@gmail.com>
2024-03-02 17:54:55 +0530

    .

--- Bit Manipulation/SwapNumbers.cpp
+++ Bit Manipulation/SwapNumbers.cpp
@@ -4,8 +4,8 @@ using namespace std;
 int main(){
     int a=5,b=7;
     cout<<a<<" "<<b<<endl;
-    a=a^b;
-    b=a^b;
-    a=a^b;
+    a=a^b; // a = 5^7
+    b=a^b;// b = 5^7^7 = 5
+    a=a^b; // a = 5^7^5 = 7
     cout<<a<<" "<<b;
 }
