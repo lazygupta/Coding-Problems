a91306528fdd9d3cfce7c43ab693eebecc739c8f
Ankit <guptashirley02@gmail.com>
2024-03-02 22:08:41 +0530

    .

--- Recursion and Backtracking/51.N-Queens.cpp
+++ Recursion and Backtracking/51.N-Queens.cpp
@@ -1,20 +1,22 @@
 // Same intuition as Word Search
 // We have to fill Q in every row and then check for valid fill that it can accomodate Q there or not
 
-
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
+    bool validfill(int col, int row, vector<string> bucket, int n)
+    {
+        int duprow = row;
+        int dupcol = col;
 
-    bool validfill(int col,int row,vector<string> bucket,int n){
-        int duprow=row;
-        int dupcol=col;
-
-        //Checking Diagonally Upward Backward
-        while(row>=0 && col>=0){
-            if(bucket[row][col] == 'Q') return false;
+        // Checking Diagonally Upward Backward
+        while (row >= 0 && col >= 0)
+        {
+            if (bucket[row][col] == 'Q')
+                return false;
             row--;
             col--;
         }
@@ -22,17 +24,21 @@ public:
         row = duprow;
         col = dupcol;
 
-        //Checking Horizontally Backward
-        while(col>=0){
-            if(bucket[row][col] =='Q') return false;
+        // Checking Horizontally Backward
+        while (col >= 0)
+        {
+            if (bucket[row][col] == 'Q')
+                return false;
             col--;
         }
         row = duprow;
         col = dupcol;
 
-        //Checking Diagonally Downward Backward
-        while(row<n && col>=0){
-            if(bucket[row][col] == 'Q') return false;
+        // Checking Diagonally Downward Backward
+        while (row < n && col >= 0)
+        {
+            if (bucket[row][col] == 'Q')
+                return false;
             row++;
             col--;
         }
@@ -40,30 +46,36 @@ public:
         return true;
     }
 
-    void solve(int col,vector<vector<string>> &res,vector<string> &bucket,int n){
-        if(col==n){
+    void solve(int col, vector<vector<string>> &res, vector<string> &bucket, int n)
+    {
+        if (col == n)
+        {
             res.push_back(bucket);
             return;
         }
 
-        int row=0;
-        for(row=0;row<n;row++){
-            if(validfill(col,row,bucket,n)){
-                bucket[row][col]='Q';
-                solve(col+1,res,bucket,n);
-                bucket[row][col]='.';
+        int row = 0;
+        for (row = 0; row < n; row++)
+        {
+            if (validfill(col, row, bucket, n))
+            {
+                bucket[row][col] = 'Q';
+                solve(col + 1, res, bucket, n);
+                bucket[row][col] = '.';
             }
         }
     }
 
-    vector<vector<string>> solveNQueens(int n) {
+    vector<vector<string>> solveNQueens(int n)
+    {
         vector<vector<string>> res;
         vector<string> bucket(n);
-        string str(n,'.'); // Filling the string with '.'
-        for(int i=0;i<n;i++){
-            bucket[i]=str; //Filling the bucket with empty string of '.'
+        string str(n, '.'); // Filling the string with '.'
+        for (int i = 0; i < n; i++)
+        {
+            bucket[i] = str; // Filling the bucket with empty string of '.'
         }
-        solve(0,res,bucket,n);
+        solve(0, res, bucket, n);
         return res;
     }
 };
