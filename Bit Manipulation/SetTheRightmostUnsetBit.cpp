31a2aa98b5511a32c0f2e988862d51692d8dbfd6
Ankit <guptashirley02@gmail.com>
2024-04-24 12:42:12 +0530

    bit manipulation

--- Bit Manipulation/SetTheRightmostUnsetBit.cpp
+++ Bit Manipulation/SetTheRightmostUnsetBit.cpp
@@ -24,3 +24,35 @@ int setBits(int n){
     int bitmask = 1<<count; // 0001 | 00111
     return n|bitmask;
 }
+
+// 2nd Approach - Bit Manipulation
+
+class Solution{
+    public:
+    vector<long long int> twoOddNum(long long int nums[], long long int n)  
+    {
+        // code here
+        int xxory = 0;
+        for (int i = 0; i < n; ++i) {
+            xxory ^= nums[i];
+        }
+        // Mask for rightmostset bit
+        int mask = xxory & -(xxory-1); // To find out rightmostsetbit, this is a mask 
+        long long int first=0,second=0;
+        for(int i = 0; i < n; ++i){
+            if(nums[i] & mask){
+                first = first ^ nums[i]; // Yaha pr wo aayega jiska rightmostbit set hoga
+            }
+            else{
+                second = second ^ nums[i];
+            }
+        }
+        // cout<<endl;
+        if(first>second){
+            return {first,second};
+        }
+        else{
+            return {second,first};
+        }
+    }
+};
--- practice.cpp
+++ practice.cpp
@@ -1,27 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int solve(int n,string str){
-    int valley=0;
-    int count=0;
-    for(auto it:str){
-        if(valley==0){
-            count++;
-        }
-        if(it=='D'){
-            valley--;
-        }
-        else{
-            valley++;
-        }
-        
-    }
-    return count-1;
-}
-
 int main(){
-    int n=8;
-    string str = "DUUUDDDU";
-    cout<<solve(n,str);
+    int n=-8;
+    cout<<~(n);
     return 0;
 }
