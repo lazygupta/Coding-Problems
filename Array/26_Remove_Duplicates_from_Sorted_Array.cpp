a4cf83321c9eb6b256f31d079f8e0eb135dbdb5e
Ankit <guptashirley02@gmail.com>
2024-06-24 03:23:43 +0530

    revision array 24 June

--- Array/26_Remove_Duplicates_from_Sorted_Array.cpp
+++ Array/26_Remove_Duplicates_from_Sorted_Array.cpp
@@ -38,3 +38,20 @@ public:
         return i+1;
     }
 };
+
+// Two Pointer Approach
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l=0,r=0;
+        while(r<nums.size()){
+            if(nums[l]!=nums[r]){
+                swap(nums[l+1],nums[r]);
+                l++;
+            }
+            r++;
+        }
+        return l+1;
+    }
+};
