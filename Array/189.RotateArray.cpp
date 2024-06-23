d9df040f1ee81835139bfb65d325c2c439edc215
Ankit <guptashirley02@gmail.com>
2024-06-24 03:37:34 +0530

    24-06 rev arrays

---
+++ Array/189.RotateArray.cpp
@@ -0,0 +1,20 @@
+// O(N)
+
+class Solution {
+public:
+
+    void reverse(vector<int> &nums,int start,int end){
+        while(start<end){
+            swap(nums[start],nums[end]);
+            start++;
+            end--;
+        }
+    }
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k = k%n;
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+};
