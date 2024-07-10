b401845650b820c0ceec6ab29f21ce75961fd168
Ankit <guptashirley02@gmail.com>
2024-07-11 00:34:04 +0530

    .

--- Recursion and Backtracking/50.Pow(x, n).cpp
+++ Recursion and Backtracking/50.Pow(x, n).cpp
@@ -26,3 +26,31 @@ public:
         return num;
     }
 };
+
+// 2nd method , Without de referencing a variable
+
+class Solution {
+public:
+
+    double answer(double x,int n){
+        if(n==0){
+            return 1;
+        }
+        return x * answer(x,n-1);
+    }
+
+    double myPow(double x, int n) {
+        if (n == INT_MAX) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
+        if (n == INT_MIN) return (x == 1 || x == -1) ? 1 : 0;
+        double num;
+        if(n>=0){
+            num = answer(x,n);
+        } 
+        else {
+            n = -n;
+            num = answer(x,n);
+            num = 1.0/num;
+        } 
+        return num;
+    }
+};
