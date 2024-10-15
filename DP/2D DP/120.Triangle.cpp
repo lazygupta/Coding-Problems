e42fa26eb00d7b95b5b75ac30e823bf7a43b9c94
Ankit Gupta <guptashirley02@gmail.com>
2024-10-15 14:35:04 +0530

    15 oct '24

--- DP/2D DP/120.Triangle.cpp
+++ DP/2D DP/120.Triangle.cpp
@@ -7,23 +7,92 @@
 class Solution {
 public:
 
-    int solve(int n,int row,int col,vector<vector<int>> &dp,vector<vector<int>> &triangle){
-        if(row == n-1){
-            return triangle[row][col];
-        }
-        if(dp[row][col] != -1) return dp[row][col];
-        int down = INT_MAX,diag = INT_MAX;
-        if(row<n) down = solve(n,row+1,col,dp,triangle);
-        if(col<triangle[n-1].size()) diag = solve(n,row+1,col+1,dp,triangle);
-        return dp[row][col] = triangle[row][col] + min(down,diag);
+    int func(vector<vector<int>>& triangle,int i,int j,int m, int n,vector<vector<int>> &dp){
+        if(j>=n || i>=m) return INT_MAX;
+        if(i == m-1 && j<n) return triangle[i][j];
+        if(dp[i][j] != -1) return dp[i][j];
+        int opt1 = func(triangle,i+1,j,m,n,dp);
+        int opt2 = func(triangle,i+1,j+1,m,n,dp);
+        return dp[i][j] = triangle[i][j] + min(opt1,opt2);
     }
 
     int minimumTotal(vector<vector<int>>& triangle) {
-        int row = 0;
-        int col = 0;
-        int n = triangle.size();
-        vector<vector<int>> dp(n,vector<int> (n,-1));
-        return solve(n,row,col,dp,triangle);
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+        if(m==1 && n==1) return triangle[0][0];
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        return func(triangle,0,0,m,n,dp);
+    }
+};
 
+
+// Tabulation Approach
+
+// 1. Suppose that only the last level is given. 
+
+// 4 1 8 3
+
+// Then what should be your answer?
+
+// If you thought 1, then congratulations 🎉 you are correct. This is our first subproblem. 
+
+// 2. Now, the last two levels are given.
+
+//  6 5 7
+// 4 1 8 3
+
+// Here, what should be the answer and what information do you need to store?
+
+// Clearly, the answer is 6 (5 -> 1). But, this may not be the optimal path. So, we need to store all the optimal paths, i.e.,
+// [(6 -> 1), (5 -> 1), 7 -> 3)] or [7, 6, 10]. This is our second subproblem.
+
+// 3. Last 3 levels are given.
+
+//   3 4
+//  6 5 7
+// 4 1 8 3
+
+// Again, what should be the answer and what information do you need to store (or use)?
+
+// Answer is 9 (3 -> 5 -> 1). Do we need to look again at all the paths? Can we use the information that we previously stored?
+// No and Yes.
+// If we replace the triangle as
+//   3 4
+//  7 6 10
+// then also, we'll get the same answer. And we can store this information as [(3 -> 6), (4 -> 6)] or [9, 10].
+
+// 4. All levels are given
+
+//    2
+//   3 4
+//  6 5 7
+// 4 1 8 3
+
+// Which can be replaced as:
+//    2
+//   9 10
+
+// And hence, our answer is 11 (2 -> 9)
+
+class Solution {
+public:
+// Tabulation Approach
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int m = triangle.size();
+        int n = triangle[m-1].size();
+        if(m==1 && n==1) return triangle[0][0];
+        vector<vector<int>> dp(m,vector<int> (n,-1));
+        for(int j=0;j<m;j++) { // We are starting from bottom 
+            dp[m-1][j] = triangle[m-1][j];
+        }
+        for(int i= m-2; i>=0 ; i--){
+            for(int j =0;j<triangle[i].size();j++){
+                int opt1 = INT_MAX,opt2 = INT_MAX;
+                opt1 = dp[i+1][j];
+                if(j<triangle[i].size()) opt2 = dp[i+1][j+1];
+                dp[i][j] = triangle[i][j] + min(opt1,opt2);
+            }
+        }
+        return dp[0][0];
     }
 };
